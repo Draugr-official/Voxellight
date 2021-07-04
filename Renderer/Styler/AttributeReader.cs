f3e01350deb8e977bbfb5c02b4fc1723a8268d94
NativeX64 <57600814+Draugr-official@users.noreply.github.com>
2021-07-05 01:52:25 +0200

    Add files via upload

---
+++ Renderer/Styler/AttributeReader.cs
@@ -0,0 +1,79 @@
+﻿using HtmlAgilityPack;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+using System.Windows.Media;
+
+namespace Voxellight.Renderer.Styler
+{
+    /// <summary>
+    /// Reads and translates attributes
+    /// </summary>
+    class AttributeReader
+    {
+        /// <summary>
+        /// Gets / sets the raw attributes
+        /// </summary>
+        HtmlAttributeCollection Attributes { get; set; }
+
+        /// <summary>
+        /// Gets / sets the translated attributes
+        /// </summary>
+        TranslatedAttributeCollection TranslatedAttributes = new TranslatedAttributeCollection();
+
+        /// <summary>
+        /// </inherit>
+        /// </summary>
+        /// <param name="Attributes"></param>
+        public AttributeReader(HtmlAttributeCollection Attributes)
+        {
+            this.Attributes = Attributes;
+        }
+
+        public TranslatedAttributeCollection TranslateAttributes()
+        {
+            foreach(HtmlAttribute Attribute in this.Attributes)
+            {
+                TranslatedAttribute translatedAttribute = new TranslatedAttribute()
+                {
+                    Name = Attribute.Name
+                };
+
+                switch(Attribute.Name)
+                {
+                    case "Margin":
+                        {
+                            if(Attribute.Value.Contains(","))
+                            {
+                                ThicknessConverter thicknessConverter = new ThicknessConverter();
+                                Thickness Result = (Thickness)thicknessConverter.ConvertFromString(Attribute.Value);
+
+                                translatedAttribute.Data = Result;
+                            }
+                            else
+                            {
+                                int Margin = Convert.ToInt32(Attribute.Value);
+                                translatedAttribute.Data = new Thickness(Margin);
+                            }
+                            break;
+                        }
+
+                    default:
+                        {
+                            translatedAttribute.Data = Attribute.Value;
+                            break;
+                        }
+                }
+
+                this.TranslatedAttributes.Add(Attribute.Name, translatedAttribute);
+            }
+
+            return this.TranslatedAttributes;
+        }
+    }
+}
---
+++ Renderer/Styler/TranslatedAttribute.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Voxellight.Renderer.Styler
+{
+    /// <summary>
+    /// Translated attribute
+    /// </summary>
+    struct TranslatedAttribute
+    {
+        /// <summary>
+        /// Name of the translated attribute
+        /// </summary>
+        public string Name { get; set; }
+
+        /// <summary>
+        /// Data of the translated attribute
+        /// </summary>
+        public object Data { get; set; }
+    }
+
+    /// <summary>
+    /// Translated attribute collection
+    /// </summary>
+    class TranslatedAttributeCollection : Dictionary<string, TranslatedAttribute>
+    {
+        /// <summary>
+        /// </inherit>
+        /// </summary>
+        public TranslatedAttributeCollection() { }
+    }
+}
